724f91ed921813cf8da12c0144656230adce3869
baofeng <baofeng_zheng@163.com>
2018-07-11 16:34:58 +0800

    complete TriangleExercises_3

---
+++ src/main/com/tw/TriangleExercises_3.java
@@ -0,0 +1,10 @@
+package main.com.tw;
+
+public class TriangleExercises_3 {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 0; i < n; ++i) {
+            System.out.println("*");
+        }
+    }
+}
