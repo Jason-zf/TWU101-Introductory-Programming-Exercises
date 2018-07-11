40423d31de005413c602b5a4bf979080e728b1e5
baofeng <baofeng_zheng@163.com>
2018-07-11 16:34:04 +0800

    complete TriangleExercises_2

---
+++ src/main/com/tw/TriangleExercises_2.java
@@ -0,0 +1,10 @@
+package main.com.tw;
+
+public class TriangleExercises_2 {
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = 0; i < n; ++i) {
+            System.out.print("*");
+        }
+    }
+}
