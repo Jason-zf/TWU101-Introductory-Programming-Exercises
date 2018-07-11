7e98f4d96c67e2d45acc0a982b241f27853ae3b9
baofeng <baofeng_zheng@163.com>
2018-07-11 20:48:55 +0800

    complete DiamondExercises_3

---
+++ src/main/com/tw/DiamondExercises_3.java
@@ -0,0 +1,26 @@
+package main.com.tw;
+
+public class DiamondExercises_3 {
+    public static void main(String[] args) {
+        int n = 3;
+        for (int i = 1; i <= n - 1; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("Bill");
+        for (int i = n - 1; i > 0; --i) {
+            for (int j = 0; j < n - i; ++j) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
